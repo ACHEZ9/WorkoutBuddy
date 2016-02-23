4f5732378e59ee4140eb23f845769b3eb3662a6d
Allan Chesarone <achesarone@gmail.com>
2016-02-23 18:44:05 -0500

    Added nav bar and messed around with some css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
     def require_login
       unless logged_in?
-        redirect_to root_path
+        redirect_to root_path, notice: "Please Login to Continue"
       end
     end
     
