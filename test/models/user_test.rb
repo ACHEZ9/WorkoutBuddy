14f654bd919f8a638b716f789cee3f6ef5d6cca3
Prayuth Naduthota <pnaduthota@gmail.com>
2016-03-02 14:40:59 -0500

    Added tests, updated header

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
-require 'test_helper'
+require 'minitest/autorun'
 
-class UserTest < ActiveSupport::TestCase
+class User < Minitest::Test
   # test "the truth" do
   #   assert true
   # end
+
+  let(:user_setup){ { name: "Dave", email: "dave@gmail.com", bio: "what's up", password: "12345678"} }
+
+  it "is valid with valid parameters" do
+    user = User.new setup
+
+    user.must_be :valid?
+  end
+
+  it "is invalid without name" do
+    parameters = setup.clone
+    parameters.delete :name
+    user = User.new parameters
+
+    user.wont_be :valid?
+    user.errors[:name].must_be :present?
+  end
+
+  it "can log in" do
+
+  end
 end
