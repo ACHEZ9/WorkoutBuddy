096f3c8bc8c8042e3707495bd49ef3cb6d9eb1ea
Prayuth Naduthota <pnaduthota@gmail.com>
2016-03-01 19:55:32 -0500

    Updated to allow for user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,14 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }, 
+  validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 4}, on: :create, unless: :reset_password?
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def reset_password?
     new_record? || password.present?
