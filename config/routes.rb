658dfa02b8e05fd53d2cf63bcc7d3f16daad8bcb
Allan Chesarone <achesarone@gmail.com>
2016-03-01 20:55:13 -0500

    added rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
