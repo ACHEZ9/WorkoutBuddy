a0b6b2d44d97bd4aea197562dd806910cfb01a05
Allan Chesarone <achesarone@gmail.com>
2016-03-02 18:11:05 -0500

    Fixed failing tests related to paperclip

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -19,7 +19,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { avatar: @user.avatar, bio: @user.bio, email: "unique@email.com", name: @user.name, password: "Test", password_confirmation: "Test"}
+      post :create, user: {bio: @user.bio, email: "unique@email.com", name: @user.name, password: "Test", password_confirmation: "Test"}
     end
 
     assert_redirected_to user_path(assigns(:user))
@@ -36,7 +36,7 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    patch :update, id: @user, user: { avatar: @user.avatar, bio: @user.bio, email: @user.email, name: @user.name, password: "Test", password_confirmation: "Test" }
+    patch :update, id: @user, user: { bio: @user.bio, email: @user.email, name: @user.name, password: "Test", password_confirmation: "Test" }
     assert_redirected_to user_path(assigns(:user))
   end
 
