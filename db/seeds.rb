4ea0afc860271afa059208a9e06d74805f6b2068
Allan Chesarone <achesarone@gmail.com>
2016-03-02 21:30:47 -0500

    Added seed data for users, events, and user_events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
 
-User.create(id: '1', name: "Brad", email: "bpayne@brandeis.edu", password: '12345678')
-User.create(id: '2', name: "Allan", email: "achesaro@brandeis.edu", password: '12345678')
-User.create(id: '3', name: "Prayuth", email: "pnadu@brandeis.edu", password: '12345678')
+User.create(name: "Brad", email: "bpayne@brandeis.edu", password: '12345678')
+User.create(name: "Allan", email: "achesaro@brandeis.edu", password: '12345678')
+User.create(name: "Prayuth", email: "pnadu@brandeis.edu", password: '12345678')
+
+# Users
+10.times do
+  name = Faker::Name.name
+  email = Faker::Internet.safe_email(name)
+  bio = Faker::Hipster.sentence
+
+  User.create(name: name, email: email, password: "deis", bio: bio)
+end
+
+# Events
+20.times do
+  name = Faker::Team.sport
+  desc = Faker::Hacker.say_something_smart
+  time = Faker::Time.between(2.days.ago, 5.days.from_now, :day)
+  location = Faker::Address.street_name
+
+  Event.create(name: name, desc: desc, time: time, location: location)
+end
+
+# Users Signup for Events, every user gets 2 events
+users = User.all
+users.each do |user|
+  #PG AND SQLITE3 SPECIFIC SYNTAX
+  user.events << Event.limit(1).order("RANDOM()")
+end
+
