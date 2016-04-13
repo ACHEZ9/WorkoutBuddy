1418cfc3766638b4291bd5b3baf7c44ae8ff233f
Allan Chesarone <achesarone@gmail.com>
2016-04-13 18:36:44 -0400

    Ajax for search

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
   $('#timepicker1').timepicker();
   $('.input-group.date').datepicker();
+  $('#sport_select').on('change', submitForm);
+  $('#distance_select').on('keyup', submitForm);
 });
+
+
+var submitForm = function() {
+  $('#events_search').submit()
+}
