8fac620db43601ba38a4d8d89b65c1200e377fe5
Prayuth Naduthota <pnaduthota@gmail.com>
2016-04-19 14:44:03 -0400

    Fixed static page routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   get 'home' => 'static_pages#home'
   get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages/about'
-  get 'contact' => 'static_pages/contact'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   resources :users do
     resources :events
