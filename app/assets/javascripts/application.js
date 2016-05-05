068c8a9f3917bb85d1172d2e144db4173ef5d8ce
Prayuth Naduthota <pnaduthota@gmail.com>
2016-05-05 09:19:37 -0400

    Ongoing UI edits

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,7 +47,7 @@ $(function collapseNavbar() {
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+})
 
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
